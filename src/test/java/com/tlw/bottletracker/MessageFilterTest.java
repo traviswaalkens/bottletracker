d287135d0bd930a8ed255831c6344a612bb582c3
Travis Waalkens <travis.waalkens@gmail.com>
2019-09-21 15:28:28 -0700

    Recreated the JUnit tests for MessageFilter and PropertyReader

---
+++ src/test/java/com/tlw/bottletracker/MessageFilterTest.java
@@ -0,0 +1,63 @@
+package com.tlw.bottletracker;
+
+import static org.junit.Assert.*;
+
+import java.io.UnsupportedEncodingException;
+
+import javax.mail.Address;
+import javax.mail.internet.InternetAddress;
+
+import org.junit.Test;
+
+public class MessageFilterTest {
+	public String subject001() {return "Bottle Event Alert"; }
+	public String subject002() {return "Diaper Event Alert"; }
+	public Address address001() throws UnsupportedEncodingException {return new InternetAddress( "noreply@kidreports.com", "KidReports Notifier" ); }
+	public Address address002() throws UnsupportedEncodingException {return new InternetAddress( "kimberly@kidreports.com", "KidReports Notifier" ); }
+	public Address address003() throws UnsupportedEncodingException {return new InternetAddress( "noreply@kidreports.com", "Kimberlyr" ); }	
+	
+	@Test
+	public void testMatches() throws UnsupportedEncodingException {
+		MessageFilter filter = new MessageFilter();
+		
+		String subject = subject001();
+		Address address = address001(); 
+		
+		assertTrue( filter.matches(subject, new Address[]{address} ));
+		
+	}
+	
+	@Test
+	public void testMatchesWithFailingSubject() throws UnsupportedEncodingException {
+		MessageFilter filter = new MessageFilter();		
+		String subject = subject002();
+		Address address = address001(); 		
+		assertFalse( filter.matches(subject, new Address[]{address} ));
+	}
+	
+	@Test
+	public void testMatchesWithFailingAddress() throws UnsupportedEncodingException {
+		MessageFilter filter = new MessageFilter();
+		
+		String subject = subject001();				
+		// verify failure isn't because of the subject
+		assertTrue( filter.matches(subject,  new Address[] {address001()}));
+		
+		// verify failure of email or name cause errors
+		assertFalse( filter.matches(subject,  new Address[] {address002()})); 		
+		assertFalse( filter.matches( subject,  new Address[] {address003()}));		
+	}
+	
+	@Test
+	public void testMatchesWithSuccessfulAddressInPosition2() throws UnsupportedEncodingException {
+		MessageFilter filter = new MessageFilter();
+		
+		String subject = subject001();
+		Address[] addresses = new Address[2];
+		addresses[0] = address002();
+		addresses[1] = address001();
+
+		assertTrue( filter.matches( subject, addresses ));		
+	}
+
+}
---
+++ src/test/java/com/tlw/bottletracker/PropertyReaderTest.java
@@ -0,0 +1,28 @@
+package com.tlw.bottletracker;
+
+import static org.junit.Assert.*;
+
+import java.io.IOException;
+import java.util.Properties;
+
+import org.junit.Test;
+
+public class PropertyReaderTest {
+
+	@Test
+	public void testGetCredentialsProperties() throws IOException {
+		PropertyReader pr = new PropertyReader();
+		Properties p = pr.getCredentialsProperties();
+		
+		assertEquals(5,p.size() );		
+	}
+
+	@Test
+	public void testGetConfigProperties() throws IOException {
+		PropertyReader pr = new PropertyReader();
+		Properties p = pr.getConfigProperties();
+		
+		assertEquals(0, p.size() );		
+	}
+
+}
